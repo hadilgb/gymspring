5acbd434e298f56172590ab4e067b15bb88fcf9b
hadil <hadilgy25@gmailo.com>
2023-02-18 16:18:36 +0100

    th

---
+++ src/test/java/com/gym/baagym/BaagymApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gym.baagym;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaagymApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
